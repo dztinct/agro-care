39900ca42f1d25d86a0da5237c7583289e892bc3
Sabeeh Ajibade <detunjinov.com>
2022-07-24 12:35:55 +0400

    backup

---
+++ controllers/register.js
@@ -0,0 +1,22 @@
+const db = require('../routes/db-config')
+const bcrypt = require('bcryptjs')
+
+const register = async (req, res) => {
+    const { firstname, lastname, email, password: Npassword } = req.body
+    if(!firstname || !lastname || !email || !Npassword) return res.json({status: 'error', error: "Please Enter complete credentials"})
+    else{
+        db.query('SELECT email FROM users WHERE email = ?', [email], async (err, result) => {
+            if(err) throw err;
+            if(result[0]) return res.json({status: 'error', error: 'Email has already been registered'})
+            else{
+                const password = await bcrypt.hash(Npassword, 8);
+                db.query('INSERT INTO users SET ?', {firstname: firstname, lastname: lastname, email: email, password : password}, (error, results) => {
+                    if(error) throw error;
+                    return res.json({ status: 'success', success: 'User has been registered'})
+                })
+            }
+        })
+    }
+}
+
+module.exports = register 
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const db = require('./routes/db-config')
+const cookie = require('cookie-parser');
+const PORT = process.env.PORT || 4000
+app.use('/js', express.static(__dirname + '/public/js'))
+app.use('/css', express.static(__dirname + '/public/css'))
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(cookie())
+app.use(express.json())
+db.connect((err) => {
+    if (err) throw err
+    console.log('Connected to database')
+})
+
+const pages = require('./routes/pages')
+
+app.use('/', pages);
+
+app.use('/api', require('./controllers/auth'))
+
+const path = require('path')
+const methodOverride = require('method-override')
+// const bodyParser = require('body-parser');
+
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended : true}))
+
+app.use(express.static(path.join(__dirname, 'assets')))
+
+
+app.listen(PORT, () => {console.log(`App running on port ${PORT}`)})
